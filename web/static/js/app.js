af9e53ea1b5d79b3d713269404e7d1421372f971
jake <jake@coffshire.com>
2013-11-03 01:58:53 -0500

    Get the job page to work and create a gorunner service.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -2,41 +2,69 @@ var app = angular.module("GoRunnerApp", [], function ($routeProvider) {
 	$routeProvider.when('/jobs', {
 		templateUrl: '/static/templates/jobs.html',
 		controller: JobsCtl
-	});
-	$routeProvider.otherwise({
+	})
+	.when('/jobs/:job', {
+		templateUrl: '/static/templates/job.html',
+		controller: JobCtl
+	})
+	.otherwise({
 		redirectTo: '/jobs'
 	});
 });
 
-app.controller('MainCtl', function ($scope, $http) {
-	$scope.getRecentRuns = function () {
+app.factory('gorunner', function($http){
+	var s = {};
+
+	s.getRecentRuns = function (scope) {
 		$http({
 			method: "GET",
 			url: "/runs"
 		})
-			.success(function (data) {
-				$scope.recent = data;
-			})
-			.error(function () {
-				$scope.recent = [];
-			})
+		.success(function (data) {
+			scope.recent = data;
+		})
+		.error(function () {
+			scope.recent = [];
+		})
 	};
-	$scope.getRecentRuns();
-});
 
-function JobsCtl($scope, $http) {
-	$scope.listJobs = function () {
+	s.listJobs = function (scope) {
 		$http({
 			method: "GET",
 			url: "/jobs"
 		})
-			.success(function (data) {
-				$scope.jobs = data;
-			})
-			.error(function () {
-				alert("Error loading jobs");
-			});
+		.success(function (data) {
+			scope.jobs = data;
+		})
+		.error(function () {
+			alert("Error loading jobs");
+		});
 	};
 
-	$scope.listJobs();
+	s.getJob = function(name, scope) {
+		$http({
+			method: "GET",
+			url: "/jobs/" + name
+		})
+		.success(function(data){
+			scope.job = data;
+		})
+		.error(function(){
+			alert("Error loading " + name);
+		})
+	};
+
+	return s;
+});
+
+app.controller('MainCtl', function ($scope, gorunner) {
+	gorunner.getRecentRuns($scope);
+});
+
+function JobsCtl($scope, gorunner) {
+	gorunner.listJobs($scope);
+}
+
+function JobCtl($scope, $routeParams, gorunner) {
+	gorunner.getJob($routeParams.job, $scope);
 }
