b4697d604633e670dc639d8319beb107aa9dce89
jake <jake@coffshire.com>
2013-11-03 10:06:53 -0600

    Tasks views working, for the most part. Not pretty yet.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,16 +1,34 @@
 var app = angular.module("GoRunnerApp", [], function ($routeProvider) {
 	$routeProvider.when('/jobs', {
+		title: "jobs",
 		templateUrl: '/static/templates/jobs.html',
 		controller: JobsCtl
 	})
 	.when('/jobs/:job', {
+		title: "job",
 		templateUrl: '/static/templates/job.html',
 		controller: JobCtl
+	})
+	.when('/tasks', {
+		title: "tasks",
+		templateUrl: '/static/templates/tasks.html',
+		controller: TasksCtl
+	})
+	.when('/tasks/:task', {
+		title: "task",
+		templateUrl: '/static/templates/task.html',
+		controller: TaskCtl
 	})
 	.otherwise({
 		redirectTo: '/jobs'
 	});
 });
+
+app.run(['$location', '$rootScope', function($location, $rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        $rootScope.title = current.$$route.title;
+    });
+}]);
 
 app.factory('gorunner', function($http){
 	return {
@@ -39,11 +57,30 @@ app.factory('gorunner', function($http){
 			})
 			.success(success)
 			.error(failure)
+		},
+
+		listTasks: function(success, failure) {
+			$http({
+				method: "GET",
+				url: "/tasks"
+			})
+			.success(success)
+			.error(failure);
+		},
+
+		getTask: function(name, success, failure) {
+			$http({
+				method: "GET",
+				url: "/tasks/" + name
+			})
+			.success(success)
+			.error(failure);
 		}
 	}
 });
 
 app.controller('MainCtl', function ($scope, gorunner) {
+	$scope.page = "jobs";
 	gorunner.getRecentRuns(function(data){
 		$scope.recent = data;
 	}, function(data) {
@@ -52,6 +89,7 @@ app.controller('MainCtl', function ($scope, gorunner) {
 });
 
 function JobsCtl($scope, gorunner) {
+	$scope.page = "jobs";
 	gorunner.listJobs(function (data) {
 		$scope.jobs = data;
 	}, function () {
@@ -60,9 +98,28 @@ function JobsCtl($scope, gorunner) {
 }
 
 function JobCtl($scope, $routeParams, gorunner) {
+	$scope.page = "job";
 	gorunner.getJob($routeParams.job, function(data){
 		$scope.job = data;
 	}, function(){
 		alert("Error loading " + name);
 	});
+}
+
+function TasksCtl($scope, gorunner) {
+	$scope.page = "tasks";
+	gorunner.listTasks(function(data) {
+		$scope.tasks = data;
+	}, function(data) {
+		alert("Error loading tasks");
+	})
+}
+
+function TaskCtl($scope, $routeParams, gorunner) {
+	$scope.page = "task";
+	gorunner.getTask($routeParams.task, function(data) {
+		$scope.task = data;
+	}, function(data) {
+		alert("Error loading task " + $routeParams.task)
+	})
 }
