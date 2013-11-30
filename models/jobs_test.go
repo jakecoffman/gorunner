6d5aef74ba7499059c8def4ca2b7287d5721cb47
jake <jake@coffshire.com>
2013-11-30 11:37:06 -0600

    a few job unit tests

---
+++ models/jobs_test.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestJobID(t *testing.T) {
+	job := Job{Name: "name"}
+	if job.ID() != "name" {
+		t.Errorf("ID() expected %s but got %s", "name", job.ID())
+	}
+}
+
+func TestJobAppendTask(t *testing.T) {
+	job := Job{"name", make([]string, 0), "status", make([]string, 0)}
+	job.AppendTask("task")
+	expected := []string{"task"}
+	if fmt.Sprintf("%#v", job.Tasks) != fmt.Sprintf("%#v", expected) {
+		t.Errorf("Expected %#v but got %#v", expected, job.Tasks)
+	}
+}
